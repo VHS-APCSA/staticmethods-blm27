2f3b10f11965a934cff00a82a13116a763c926c1
littl <littl@DESKTOP-IK0GM3J.attlocal.net>
2017-12-07 18:18:25 -0800

    Finished StaticMethods assignment

--- src/StaticMethods.java
+++ src/StaticMethods.java
@@ -8,9 +8,27 @@ public class StaticMethods
 	}
 	public static double coneVolume(double height, double radius)
 	{
-		double cone = ((1/3)(Math.PI)(radius*radius)(height));
+		double cone = ((1.0/3.0)*(Math.PI)*(radius*radius)*(height));
 		return cone;
 	}
+	public static double temperature(double fahrenheit)
+	{
+		double celcius = (fahrenheit - 32)*(5.0/9.0);
+		return celcius;
+	}
+	public static int reverse(int number)
+	{
+		int result = 0;
+		int rem;
+		while (number > 0) 
+		{
+			rem = number % 10;
+			number = number / 10;
+			result = result * 10 + rem;
+		}
+		return result;
+	}
+	
 }
 
 
---
+++ src/StaticMethodsRunner.java
@@ -0,0 +1,12 @@
+
+public class StaticMethodsRunner {
+	 public static void main(String[] args) 
+	 {
+		 System.out.println("The area of a circle with a radius of 4 is: " + StaticMethods.area(4) + "\n");
+		 System.out.println("The volume of a cone with a height of 4 and a radius of 8 is: " + StaticMethods.coneVolume(4,8) + "\n");
+		 System.out.println("The temperature of 98 degrees farehnheit to celius is: " + StaticMethods.temperature(98) + "\n");
+		 System.out.println("654 reversed is: " + StaticMethods.reverse(654) + "\n");
+
+		 
+	 }
+}
